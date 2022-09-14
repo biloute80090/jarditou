94b1b7b95266664ec49ea7a20a2418bc5b8a87c6
biloute80090 <johnnylaurie@hotmail.fr>
2022-09-14 16:33:37 +0200

    mise a jour

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,9 +1,98 @@
-console.log("texte affiché dans la console");
-document.write("hfdgjkdfgh"); 
+/console.log("texte affiché dans la console");
+document.write("votre âge");
 var reponse1 = window.prompt("Saisissez votre nom");
 var reponse2 = window.prompt("Saisissez votre prenom");
 if (window.confirm("etre-vous un homme ?") == true) 
 { 
    var sex="homme"
+   
 }
-window.alert("Bonjour "+sex+" "+reponse1+" "+reponse2+"\n\nBienvenue sur notre site web !")
+window.alert("Bonjour "+sex+" "+reponse1+" "+reponse2+"\n\nBienvenue sur notre site web !")*/
+
+
+/*var a = "100";
+var b = 100;
+var c = 1 .toFixed(2);/
+/c=c.toFixed(2) ;/
+/var d = true;
+window.alert("Ceci est une chaîne de caractères :"+a);
+b-- ;
+c = c+a ;
+d=false ;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)*/
+/*var age = 19;
+
+(age >= 18) ? console.log("Vous êtes majeur") : console.log("Vous êtes mineur");/*
+
+/*var nombre = window.prompt("Donnez un nombre") ;
+if (nombre%2 == 0 )
+{
+    window.alert("Nombre pair") ;
+}
+else
+{
+    window.alert("Nombre impair")
+}*/
+ 
+/*var date = window.prompt("Année de naissance") ;
+var age = 2022-date;
+if (age>= 18)
+{
+    window.alert("Vous avez "+age+ " ans majeur");
+}
+else
+{
+    window.alert("Tu as "+age+" ans mineur") ;
+}*/
+/*var nb = "8";
+console.log(nb)
+console.log(typeof nb)
+var age = window.prompt("ton age") ;
+console.log(age)
+console.log(typeof age)
+age = Number(age)
+console.log(age)
+console.log(typeof age)*/
+
+var nbr1 = window.prompt("Donnez un nombre1") ;
+var op = window.prompt("operateur")
+var nbr2 = window.prompt("Donnez un nombre2") ;
+console.log(nbr1)
+console.log(nbr2)
+console.log(op)
+nbr1 = Number(nbr1)
+nbr2 = Number(nbr2)
+console.log(nbr1)
+console.log(nbr2)
+switch (op)
+{
+  case "+" :
+     var resultat = nbr1+nbr2 ;
+     console.log(resultat)
+     break; 
+
+  case "-" :
+     var resultat = nbr1-nbr2 ;
+     console.log(resultat)
+     break;
+
+  case "":
+     var resultat = nbr1nbr2 ;
+     console.log(resultat)
+     break;
+  case "/":
+     var resultat = nbr1/nbr2 ;
+     console.log(resultat)
+     if (op="/" && nbr2 == 0)
+  {
+    window.alert("error");
+  }
+        break;
+  default:
+     window.alert("error");
+
+
+}
