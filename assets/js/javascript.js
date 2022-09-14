e218af1647c058941073a7fd025c79607f8dc154
biloute80090 <johnnylaurie@hotmail.fr>
2022-09-14 11:25:19 +0200

    mise a jour

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,9 @@
-<script>alert("mon premier script en javascript !");</script>
+console.log("texte affiché dans la console");
+document.write("hfdgjkdfgh"); 
+var reponse1 = window.prompt("Saisissez votre nom");
+var reponse2 = window.prompt("Saisissez votre prenom");
+if (window.confirm("etre-vous un homme ?") == true) 
+{ 
+   var sex="homme"
+}
+window.alert("Bonjour "+sex+" "+reponse1+" "+reponse2+"\n\nBienvenue sur notre site web !")
